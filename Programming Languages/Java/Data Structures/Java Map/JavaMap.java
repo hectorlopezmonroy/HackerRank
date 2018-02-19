6c577868dc2aa28d9570c292a839777caba04b50
Hector Lopez Monroy <hlope048@fiu.edu>
2018-02-19 13:25:33 -0500

    Adds solution to the 'Java Map' challenge in the Java track.

---
+++ Programming Languages/Java/Data Structures/Java Map/JavaMap.java
@@ -0,0 +1,91 @@
+/*
+ * You are given a phone book that consists of people's names and their phone
+ * number. After that you will be given some person's name as query. For each
+ * query, print the phone number of that person.
+ *
+ * Input Format
+ *
+ * The first line will have an integer 'n' denoting the number of entries in the
+ * phone book. Each entry consists of two lines: a name and the corresponding
+ * phone number.
+ *
+ * After these, there will be some queries. Each query will contain a person's
+ * name. Read the queries until end-of-file.
+ *
+ * Constraints
+ *
+ * A person's name consists of only lower-case English letters and it may be in
+ * the format 'first-name last-name' or in the format 'first-name'. Each phone
+ * number has exactly 8 digits without any leading zeros.
+ *
+ * 1 <= n <= 100000
+ *
+ * 1 <= Query <= 100000
+ *
+ * Output Format
+ *
+ * For each case, print "Not found" if the person has no entry in the phone
+ * book. Otherwise, print the person's name and phone number. See sample output
+ * fot the exact format.
+ *
+ * To make the problem easier, we provided a portion of the code in the editor.
+ * You can either complete that code or write completely on your own.
+ *
+ * Sample Input
+ *
+ * 3
+ * uncle sam
+ * 99912222
+ * tom
+ * 11122222
+ * harry
+ * 12299933
+ * uncle sam
+ * uncle tom
+ * harry
+ *
+ * Sample Output
+ *
+ * uncle sam=99912222
+ * Not found
+ * harry=12299933
+ */
+
+import java.util.Scanner;
+import java.util.HashMap;
+import java.util.LinkedList;
+
+public class JavaMap {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        HashMap<String, String> phoneBook = new HashMap<String, String>();
+        int n = in.nextInt();
+        LinkedList<String> res = new LinkedList<String>();
+        in.nextLine();
+
+        // Populating phonebook
+        for (int i = 0; i < n; i++) {
+            String name = in.nextLine();
+            String phone = in.nextLine();
+
+            phoneBook.put(name, phone);
+        }
+
+        // Process queries
+        while (in.hasNext()) {
+            String name1 = in.nextLine();
+            if (phoneBook.containsKey(name1)) {
+                res.add(name1 + "=" + phoneBook.get(name1));
+            } else {
+                res.add("Not found");
+            }
+        }
+        in.close();
+
+        // Print results for queries
+        for (String s : res) {
+            System.out.println(s);
+        }
+    }
+}
