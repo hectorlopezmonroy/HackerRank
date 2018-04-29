8f492fdc8922d1d2de18903abb0c4569ef62f743
Hector Lopez Monroy <hlope048@fiu.edu>
2018-04-29 16:10:57 -0400

    Formats code before merging to master branch.

--- Tutorials/30 Days of Code/Day 21: Generics/Generics.java
+++ Tutorials/30 Days of Code/Day 21: Generics/Generics.java
@@ -36,7 +36,7 @@ class Printer <T> {
      * Method Name: printArray
      * Print each element of the generic array on a new line. Do not return
      * anything.
-     * @param A generic array
+     * @param array A generic array
      **/
     public void printArray(T[] array) {
         for (T elem : array) {
