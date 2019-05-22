864789d22e0263eccaf00c99074e2ee3f344b83f
Hector Lopez Monroy <hlope048@fiu.edu>
2019-05-22 13:07:34 -0400

    Add solution to the 'Population Density Difference' challenge in the SQL track.

---
+++ Specialized Skills/SQL/Aggregation/PopulationDensityDifference.sql
@@ -0,0 +1,17 @@
+/*
+ * Query the difference between the maximum and minimum populations in CITY.
+ *
+ * Input Format
+ *
+ * The CITY table is described as follows:
+ *
+ *                                   CITY
+ *                             Field   |     Type
+ *                         ID          | NUMBER
+ *                         NAME        | VARCHAR2(17)
+ *                         COUNTRYCODE | VARCHAR2(3)
+ *                         DISTRICT    | VARCHAR2(20)
+ *                         POPULATION  | NUMBER
+ */
+
+SELECT MAX(POPULATION) - MIN(POPULATION) FROM CITY;
