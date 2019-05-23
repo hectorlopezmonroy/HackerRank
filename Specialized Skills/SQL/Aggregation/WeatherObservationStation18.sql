4233c1c1b3076f7f079d14fb258710399ff80c73
Hector Lopez Monroy <hlope048@fiu.edu>
2019-05-22 22:32:04 -0400

    Add solution to the 'Weather Observation Station 18' challenge in the SQL track.

---
+++ Specialized Skills/SQL/Aggregation/WeatherObservationStation18.sql
@@ -0,0 +1,41 @@
+/*
+ * Consider P1(a, b) and P2(c, d) to be two points on a 2D plane.
+ *
+ *   * 'a' happens to equal the minimum value in Northern Latitude (LAT_N in
+ *     'STATION').
+ *
+ *   * 'b' happens to equal the minimum value in Western Longitude (LONG_W in
+ *     'STATION').
+ *
+ *   * 'c' happens to equal the maximum value in Northern Latitude (LAT_N in
+ *     'STATION').
+ *
+ *   * 'd' happens to equal the maximum value in Western Longitude (LONG_W in
+ *     'STATION').
+ *
+ * Query the Manhattan Distance between points P1 and P2 and round it to a scale
+ * of 4 decimal places.
+ *
+ * Input Format
+ *
+ * The 'STATION' table is described as follows:
+ *
+ *                                  STATION
+ *                               Field |    Type
+ *                              ID     | NUMBER
+ *                              CITY   | VARCHAR2(21)
+ *                              STATE  | VARCHAR2(2)
+ *                              LAT_N  | NUMBER
+ *                              LONG_W | NUMBER
+ *
+ * where LAT_N is the northern latitude and LONG_W is the western longitude.
+ */
+
+/* Definition of Manhattan Distance:
+ * The distance between two points measured along axes at right angles.
+ * In a plane with p1 at (x1, y1) and p2 at (x2, y2), it is
+ * |x1 - x2| + |y1 - y2|.
+ */
+
+SELECT ROUND(ABS(MIN(LAT_N) - MAX(LAT_N)) + ABS(MIN(LONG_W) - MAX(LONG_W)), 4)
+FROM STATION;
