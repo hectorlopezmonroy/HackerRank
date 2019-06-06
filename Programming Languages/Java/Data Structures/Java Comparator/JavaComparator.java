95da45b7f8582e1c97ed82a03ee3106f001a76b1
Hector Lopez Monroy <hlope048@fiu.edu>
2019-06-06 19:46:30 -0400

    Code Review: Clean code for the 'Java Comparator' challenge's solution.

--- Programming Languages/Java/Data Structures/Java Comparator/JavaComparator.java
+++ Programming Languages/Java/Data Structures/Java Comparator/JavaComparator.java
@@ -2,11 +2,11 @@
  * Comparators are used to compare two objects. In this challenge, you'll create
  * a comparator and use it to sort an array.
  *
- * The 'Player' class is provided for you in your editor. It has 2 fields: a
+ * The 'Player' class is provided for you in your editor. It has '2' fields: a
  * 'name' String and a 'score' integer.
  *
  * Given an array of 'n Player' objects, write a comparator that sorts them in
- * order of decreasing score; if 2 or more players have the same score, sort
+ * order of decreasing score; if '2' or more players have the same score, sort
  * those players alphabetically by name. To do this, you must create a 'Checker'
  * class that implements the 'Comparator' interface, then write an
  * 'int compare(Player a, Player b)' method implementing the
@@ -18,16 +18,17 @@
  * class.
  *
  * The first line contains an integer, 'n', denoting the number of players.
+ *
  * Each of the 'n' subsequent lines contains a player's 'name' and 'score',
  * respectively.
  *
  * Constraints
  *
- * 0 <= 'score' <= 1000
+ * 0 <= score <= 1000
  *
- * 2 players can have the same name.
+ * * 2 players can have the same name.
  *
- * Player names consist of lowercase English letters.
+ * * Player names consist of lowercase English letters.
  *
  * Output Format
  *
