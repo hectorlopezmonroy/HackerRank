260801b2a8d1e06bc6ad6a681b8cc802eb916f56
Hector Lopez Monroy <hlope048@fiu.edu>
2019-06-12 13:58:46 -0400

    Add solution to the 'Ruby Control Structures - Until' challenge in the Ruby track.

--- Programming Languages/Ruby/Control Structures/Ruby Control Structures - Infinite Loop/Solution.rb
+++ Programming Languages/Ruby/Control Structures/Ruby Control Structures - Infinite Loop/Solution.rb
@@ -1,7 +1,7 @@
 # This is a wonderful exercise which explains the concept of infinite loops.
 #
 # "A hacker practices on HackerRank until getting to a rating of O(1) read as
-# (Oh-one)"
+# (Oh-one)".
 #
 # An infinite loop in Ruby is of the form
 #
