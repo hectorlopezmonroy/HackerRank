14ae66ced5893ab8dc1e69693db98e5cdec41748
Hector Lopez Monroy <hlope048@fiu.edu>
2019-06-12 11:57:08 -0400

    Add solution to the 'Ruby Tutorial - Everything is an Object' in the Ruby track.

---
+++ Programming Languages/Ruby/Introduction/Ruby Tutorial - Everything is an Object/Solution.rb
@@ -0,0 +1,10 @@
+# Everything is an object in Ruby. For instance, if you type
+#
+#   print self
+#
+# in the code editor, Ruby treats 'self' as the object in which it is currently
+# referred to.
+#
+# Give it a try!
+
+print self
