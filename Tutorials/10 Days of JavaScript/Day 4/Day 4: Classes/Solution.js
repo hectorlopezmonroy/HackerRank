f1a583bc0a0b935c8c12df178019f2585e879333
Hector Lopez Monroy <hlope048@fiu.edu>
2019-07-09 12:54:09 -0400

    Code Review: Clean code for the 'Day 4: Classes' challenge's solution.

--- Tutorials/10 Days of JavaScript/Day 4/Day 4: Classes/Solution.js
+++ Tutorials/10 Days of JavaScript/Day 4/Day 4: Classes/Solution.js
@@ -44,10 +44,13 @@
  * 1. A constructor that takes an array of integer side lengths.
  * 2. A 'perimeter' method that returns the sum of the Polygon's side lengths.
  */
-function Polygon(lengths) {
-    this.lengths = lengths;
+class Polygon {
 
-    this.perimeter = function perimeter() {
+    constructor(lengths) {
+        this.lengths = lengths;
+    }
+
+    perimeter() {
         let res = 0;
 
         for (let i in this.lengths) {
